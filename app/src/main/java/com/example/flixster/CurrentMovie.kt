4aa7d42aa07e72fa592f7de6a1245d8d3204827b
cae26 <cae26@njit.edu>
2023-02-13 09:55:47 -0500

    layout fix

--- app/src/main/java/com/example/flixster/CurrentMovie.kt
+++ app/src/main/java/com/example/flixster/CurrentMovie.kt
@@ -8,9 +8,9 @@ class CurrentMovie {
     @SerializedName("title")
     var title: String? = null
 
-    @JvmField
+  /*  @JvmField
     @SerializedName("release_date")
-    var releaseDate: String? = null
+    var releaseDate: String? = null*/
 
     @SerializedName("overview")
     var overview: String? = null
--- app/src/main/java/com/example/flixster/CurrentMoviesFragment.kt
+++ app/src/main/java/com/example/flixster/CurrentMoviesFragment.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.core.widget.ContentLoadingProgressBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.RequestParams
@@ -32,7 +33,7 @@ class CurrentMoviesFragment: Fragment(), OnListFragmentInteractionListener  {
         val progressBar = view.findViewById<View>(R.id.progress) as ContentLoadingProgressBar
         val recyclerView = view.findViewById<View>(R.id.list) as RecyclerView
         val context = view.context
-        recyclerView.layoutManager = GridLayoutManager(context, 2)
+        recyclerView.layoutManager = LinearLayoutManager(context)
         updateAdapter(progressBar, recyclerView)
         return view
     }
@@ -56,7 +57,7 @@ class CurrentMoviesFragment: Fragment(), OnListFragmentInteractionListener  {
                      * HTTP response status is "200 OK"
                      */
                     override fun onSuccess(
-                        statusCode: Int, headers: Headers, json: JSON
+                        statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON
                     ) {
                         // The wait for a response is over
                         progressBar.hide()
